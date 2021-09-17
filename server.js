207700e9ec6584d5ca366f554b9baee0c1f7a9ea
Akshay-1003 <akshaypatil.ap1003@gmail.com>
2021-09-18 00:16:28 +0530

    all set

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const path= require('path');
 var app = express();
 const PORT=process.env.PORT || 5000;
-var app_path = '../dist/my-app'
+var app_path = './dist/my-app'
 
 app.use('/',express.static(path.join(__dirname,app_path))).listen(PORT,()=>console.log(`Listening on ${PORT} `));
